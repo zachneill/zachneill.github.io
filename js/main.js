bd798416b9ae6723e819c781c341b869856abe1b
Zach Neill <zacharyneill@gmail.com>
2021-12-12 14:38:10 -0500

    Add local JQ and BS5 and revealer

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
+$(document).ready(function(){
+  setTimeout(reveal, 500);
+  animateCSS(".main-title", 'bounceInUp').then((message) => {
+    animateCSS(".main-title", 'pulse');
+  });
+  animateCSS(".bullet-1", "lightSpeedInLeft");
+  animateCSS(".bullet-2", "lightSpeedInRight");
+
+});
+
 const animateCSS = (element, animation, prefix = 'animate__') => new Promise((resolve, reject) => {
+  
   const animationName = `${prefix}${animation}`;
   const node = document.querySelector(element);
   node.classList.add(`${prefix}animated`, animationName);
+  $(element).removeClass("opacity-0");
   function handleAnimationEnd(event) {
     event.stopPropagation();
     node.classList.remove(`${prefix}animated`, animationName);
@@ -10,8 +22,6 @@ const animateCSS = (element, animation, prefix = 'animate__') => new Promise((re
   node.addEventListener('animationend', handleAnimationEnd, {once: true});
 });
 
-animateCSS(".main-title", 'bounceInUp').then((message) => {
-  animateCSS(".main-title", 'pulse');
-});
-animateCSS(".bullet-1", "lightSpeedInLeft");
-animateCSS(".bullet-2", "lightSpeedInRight");
+function reveal(){
+  $(".revealer").addClass("transitionOut");
+}
