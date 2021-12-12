b05050580d963d304de4eda30eb03c65b7376884
Zach Neill <zacharyneill@gmail.com>
2021-12-12 17:07:09 -0500

    Work on exit, add context cards

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
+  gone = false;
   setTimeout(reveal, 500);
   animateCSS(".main-title", 'bounceInUp').then((message) => {
     animateCSS(".main-title", 'pulse');
   });
   animateCSS(".bullet-1", "lightSpeedInLeft");
   animateCSS(".bullet-2", "lightSpeedInRight");
-
 });
 
 const animateCSS = (element, animation, prefix = 'animate__') => new Promise((resolve, reject) => {
@@ -25,3 +25,23 @@ const animateCSS = (element, animation, prefix = 'animate__') => new Promise((re
 function reveal(){
   $(".revealer").addClass("transitionOut");
 }
+// $( window ).scroll(function() {  // if relatively off screen
+//   if ($('.main-title').offset().top < ($(window).scrollTop() + ($(window).height() / 3)) && (gone==false)) {
+//     gone = true;
+//     animateCSS(".main-title", "bounceOutDown").then((message) => {
+//       $(".main-title").addClass("opacity-0");
+//     });
+//     animateCSS(".bullet-1", "lightSpeedOutRight").then((message) => {
+//       $(".bullet-1").addClass("opacity-0");
+//     });
+//     animateCSS(".bullet-2", "lightSpeedOutLeft").then((message) => {
+//       $(".bullet-2").addClass("opacity-0");
+//     });
+//   } else if (gone==true) {
+//     animateCSS(".main-title", 'bounceInUp').then((message) => {
+//       animateCSS(".main-title", 'pulse');
+//     });
+//     animateCSS(".bullet-1", "lightSpeedInLeft");
+//     animateCSS(".bullet-2", "lightSpeedInRight");
+//   };
+// });
