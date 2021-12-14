7c4785497048dc9f5e962192fc92c5600a9ee153
Zach Neill <zacharyneill@gmail.com>
2021-12-14 03:31:33 -0500

    Fade in about page

--- js/about.js
+++ js/about.js
@@ -5,13 +5,28 @@ $(document).ready(function(){
   animateCSS(".about-me", 'bounceInUp').then((message) => {
     animateCSS(".about-me", 'pulse');
   });  
-  setTimeout(function(){ animateCSS(".footer", "bounceInUp") }, 500);
   setTimeout(function(){ animateCSS(".bq-div", "fadeIn") }, 900);
+  setTimeout(function(){ animateCSS(".recognition", "fadeIn") }, 1100);
+  setTimeout(function(){ animateCSS(".skills", "fadeIn") }, 1200);
   wait = 1000;
-  for (let i = 1; i<=30; i++) {
+  for (let i = 1; i<=43; i++) {
     setTimeout(function() { animateCSS(".btn"+i, "bounceInUp") }, wait);
     wait+=50;
+    if (i === 30){
+      wait = 3050;
+    };
   };
+  setTimeout(function(){ animateCSS(".education", "fadeIn") }, 2300);
+  setTimeout(function(){ animateCSS(".college", "fadeIn") }, 2400);
+  setTimeout(function(){ animateCSS(".major", "fadeIn") }, 2500);
+  setTimeout(function(){ animateCSS(".coursework", "fadeIn") }, 2600);
+  setTimeout(function(){ animateCSS(".hobbies", "fadeIn") }, 2900);
+  setTimeout(function(){ animateCSS(".person", "fadeIn") }, 3000);
+  setTimeout(function(){ animateCSS(".additional", "fadeIn") }, 3700);
+  setTimeout(function(){ animateCSS(".snaratin", "fadeIn") }, 3800);
+  setTimeout(function(){ animateCSS(".spam", "fadeIn") }, 3900);
+  setTimeout(function(){ animateCSS(".footer", "bounceInUp") }, 4000);
+  
   var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
