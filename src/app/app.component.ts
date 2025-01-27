d484d659cb7016e081b09e8ea96230f6aa2b203a
Zach Neill <neillz@berea.edu>
2025-01-27 15:39:54 -0600

    v2 bc im a devops girlie now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {NavbarComponent} from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-
+  title = 'zachneill.github.io';
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,33 +1,11 @@
 import { Component } from '@angular/core';
-import {Global} from "../global";
-import {faLinkedin} from "@fortawesome/free-brands-svg-icons";
-import {faGithub} from "@fortawesome/free-brands-svg-icons";
-import {faFacebook} from "@fortawesome/free-brands-svg-icons";
-import {faGoogle} from "@fortawesome/free-brands-svg-icons";
-import {faDiscord} from "@fortawesome/free-brands-svg-icons";
-import {faInstagram} from "@fortawesome/free-brands-svg-icons";
-import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
+  imports: [],
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.sass']
+  styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  faLinkedin = faLinkedin
-  faGithub = faGithub
-  faFacebook= faFacebook
-  faGoogle = faGoogle
-  faDiscord = faDiscord
-  faInstagram = faInstagram
 
-  constructor(public global: Global, public router: Router){
-  }
-
-  reset() {
-    if (this.global.imageName != 'cliff'){
-      this.global.changeImage('cliff')
-      this.router.navigate(["/"]).then();
-    }
-  }
 }
